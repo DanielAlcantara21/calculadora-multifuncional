a37618940f7e6c07952bcb4271c4b9321770b32a
Daniel Alcântara <daniel_alcantara123@hotmail.com>
2025-03-27 10:14:21 -0300

    feature: Criação do header e configuração das rotas para página inicial

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,13 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { HeaderComponent } from './pagina-inicial/header/header.component';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender, // Isto indica que a página deve ser pré-renderizada
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender // Página de fallback
   }
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './pagina-inicial/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HeaderComponent, // Esta é a sua página inicial
+    pathMatch: 'full'
+  },
+  // Outras rotas podem ser configuradas aqui
+];
+
